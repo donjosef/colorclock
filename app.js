939b18da1b2896ce2ae1b6800b5eb1454d32e521
donjoe <giumontanaro@gmail.com>
2018-12-02 11:55:12 +0100

    Convert rgb values in hex

--- app.js
+++ app.js
@@ -1,12 +1,20 @@
 const current_col = document.querySelector('.current-color');
 
+function rgbToHex(...rgb) {
+    return rgb.map(val => {
+        const hex = val.toString(16);
+        return hex.length == 1 ? '0' + hex : hex;
+    }).join("");
+}
+
+
  function changeColor(h, m, s) {
     var red = Math.round(255 * ( h/23 ));
     var green = Math.round(255 * ( m/59 ));
     var blue = Math.round(255 * ( s/59 ));
 
     document.body.style.backgroundColor = `rgb(${red}, ${green}, ${blue})`;
-    current_col.innerHTML = `rgb(${red}, ${green}, ${blue})`;
+    current_col.innerHTML = `rgb(${red}, ${green}, ${blue})` + '<br>' + 'Exadecimal: ' + '#' + rgbToHex(red, green, blue);
  }
 
 function leadingZero(val) {
